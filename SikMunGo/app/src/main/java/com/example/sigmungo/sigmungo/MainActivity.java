1e3a77e105cd5009a84c4245a906bd7f2bde90e4
geni429 <geni429@gmail.com>
2017-07-24 14:06:36 +0900

    프로젝트 폴더 생성

---
+++ SikMunGo/app/src/main/java/com/example/sigmungo/sigmungo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sigmungo.sigmungo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
