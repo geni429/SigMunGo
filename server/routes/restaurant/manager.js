e3338cc55672b76d23b4b563f18d30e49acac8a9
SeoYH1017 <yunho10775@gmail.com>
2017-07-25 16:59:19 +0900

    restaurant

---
+++ server/routes/restaurant/manager.js
@@ -0,0 +1,19 @@
+let conn = require('../../DBConnection');
+
+let manager = {}
+
+manager.addRestaurant = function (contentId, name, place, phone, manu, callback) {
+
+}
+
+manager.getRestaurant = function (contentId, callback) {
+
+}
+
+manager.updateRestaurant = function (contentId, name, place, phone, manu, callback) {
+
+}
+
+manager.deleteRestaurant = function (contentId, callback) {
+
+}
--- server/routes/restaurant/router.js
+++ server/routes/restaurant/router.js
@@ -2,35 +2,35 @@ let express = require('express');
 let router = express.Router();
 let manager = require('./manager');
 
-router.route('/restaurant/:contentId').post(function(req,res){
-    let contentId=req.param.contentId;
-    let name=req.body.name;
-    let phone=req.body.phone;
-    let place=req.body.place;
-    let manu=req.body.manu;
+router.route('/restaurant/:contentId').post(function (req, res) {
+    let contentId = req.param.contentId;
+    let name = req.body.name;
+    let place = req.body.place;
+    let phone = req.body.phone;
+    let manu = req.body.manu;
 
 });
 
-router.route('/restaurant/:contentId').get(function(req,res){
-    let contentId=req.param.contentId;
+router.route('/restaurant/:contentId').get(function (req, res) {
+    let contentId = req.param.contentId;
 
 });
 
-router.route('/restaurant/:contentId').put(function(req,res){
-    let contentId=req.param.contentId;
-    let name=req.body.name;
-    let phone=req.body.phone;
-    let place=req.body.place;
-    let manu=req.body.manu;
+router.route('/restaurant/:contentId').put(function (req, res) {
+    let contentId = req.param.contentId;
+    let name = req.body.name;
+    let place = req.body.place;
+    let phone = req.body.phone;
+    let manu = req.body.manu;
 
 });
 
-router.route('/restaurant/:contentId').delete(function(req,res){
-    let contentId=req.param.contentId;
+router.route('/restaurant/:contentId').delete(function (req, res) {
+    let contentId = req.param.contentId;
 
 });
 
-router.route('/restaurant/:areaCode').get(function(req,res){
-    let areaCode=req.param.areaCode;
-    http://jojoldu.tistory.com/163
+router.route('/restaurant/:areaCode').get(function (req, res) {
+    let areaCode = req.param.areaCode;
+    http: //jojoldu.tistory.com/163
 });
