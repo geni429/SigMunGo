49e1633200f4e18b806005948c58b034ba87efa3
geni429 <geni429@gmail.com>
2017-07-25 21:22:07 +0900

    SignIn 레이아웃 개선 및 각종 네이밍 수정

---
+++ SigMunGo/app/src/main/java/com/example/sigmungo/sigmungo/Certification.java
@@ -0,0 +1,16 @@
+package com.example.sigmungo.sigmungo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by geni on 2017. 7. 25..
+ */
+
+public class Certification extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.certification);
+    }
+}
