04ef216af11ec8968ad52563da54503459572c36
geni429 <geni429@gmail.com>
2017-07-26 12:33:06 +0900

    SignIn -> SignUp 연결

--- SigMunGo/app/src/main/java/com/example/sigmungo/sigmungo/SignIn.java
+++ SigMunGo/app/src/main/java/com/example/sigmungo/sigmungo/SignIn.java
@@ -1,17 +1,23 @@
 package com.example.sigmungo.sigmungo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by dsm2017 on 2017-07-25.
  */
 
 public class SignIn extends AppCompatActivity {
+    TextView signUp;
+
     protected void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.signin);
+        signUp = (TextView)findViewById(R.id.signup);
     }
 
     @Override
@@ -23,6 +29,10 @@ public class SignIn extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void signUp(View view){
+        startActivity(new Intent(getApplicationContext(), SignUp.class));
     }
 }
 
