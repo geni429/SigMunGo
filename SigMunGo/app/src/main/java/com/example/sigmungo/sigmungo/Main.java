9a93f00b94c0e22f22fe6d731ab2073a0cf24bf7
geni429 <geni429@gmail.com>
2017-07-31 15:30:31 +0900

    main_sidemenu header구현

--- SigMunGo/app/src/main/java/com/example/sigmungo/sigmungo/Main.java
+++ SigMunGo/app/src/main/java/com/example/sigmungo/sigmungo/Main.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -21,8 +23,9 @@ import java.util.List;
  * Created by geni on 2017. 7. 26..
  */
 
-public class Main extends Activity {
+public class Main extends AppCompatActivity {
     public RecyclerView recyclerView;
+    private DrawerLayout mDrawerLayout;
     ViewPager pager;
 
     @Override
@@ -35,6 +38,9 @@ public class Main extends Activity {
         PagerThread thread = new PagerThread();
         thread.start();
         initData();
+
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+
     }
 
     public void setLocation(View v){
